07336660f5f09857c765f88bdcfe2682a1dfe4d2
pecopeco <yangpeco@qq.com>
2020-06-26 10:15:32 +0800

    增加登录页

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Provider } from "mobx-react"
 import store from './store'
 
+import Login from './views/login'
 import Home from './views/home'
 import My from './views/my'
 import Detail from './views/home/detail'
 import Setting from './views/home/setting'
 
+let StackNavigator = createStackNavigator().Navigator
+let StackScreen = createStackNavigator().Screen
+let TabNavigator = createBottomTabNavigator().Navigator
+let TabScreen = createBottomTabNavigator().Screen
+
 const HomeStackScreen = () => {
-  let StackNavigator = createStackNavigator().Navigator
-  let StackScreen = createStackNavigator().Screen
   return (pug`
     StackNavigator
       StackScreen(name="Home" component=Home options={headerShown: false, headerTitle: ''})
@@ -23,8 +27,6 @@ const HomeStackScreen = () => {
 }
 
 const App = () => {
-  let TabNavigator = createBottomTabNavigator().Navigator
-  let TabScreen = createBottomTabNavigator().Screen
   const Icon = (src) => {
     return (pug`
       Image(style={width: 25, height: 25} source=src)
@@ -33,22 +35,26 @@ const App = () => {
   return (pug`
     Provider(...store)
       NavigationContainer
-        TabNavigator(
-          screenOptions=({ route }) => ({ tabBarVisible: !route.state || route.state.index === 0 })
-          tabBarOptions={ activeTintColor: '#00dbde', inactiveTintColor: 'gray' }
-        )
-          TabScreen(name="Home" component=HomeStackScreen options={
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ focused }) => {
-              return Icon(focused ? require('./assets/home-check.png') : require('./assets/home.png'))
-            }
-          })
-          TabScreen(name="My" component=My options={
-            tabBarLabel: 'My',
-            tabBarIcon: ({focused}) => {
-              return Icon(focused ? require('./assets/my-check.png') : require('./assets/my.png'))
-            }
-          })
+        if 0 > 1
+          TabNavigator(
+            screenOptions=({ route }) => ({ tabBarVisible: !route.state || route.state.index === 0 })
+            tabBarOptions={ activeTintColor: '#00dbde', inactiveTintColor: 'gray' }
+          )
+            TabScreen(name="Home" component=HomeStackScreen options={
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ focused }) => {
+                return Icon(focused ? require('./assets/home-check.png') : require('./assets/home.png'))
+              }
+            })
+            TabScreen(name="My" component=My options={
+              tabBarLabel: 'My',
+              tabBarIcon: ({focused}) => {
+                return Icon(focused ? require('./assets/my-check.png') : require('./assets/my.png'))
+              }
+            })
+        else
+          StackNavigator
+            StackScreen(name="Login" component=Login options={headerShown: false, headerTitle: '登录'})
   `)
 }
 
---
+++ src/views/login.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text, StatusBar } from 'react-native'
+import { observer, inject } from 'mobx-react'
+
+@inject(['userInfo'])
+@observer
+class Login extends Component {
+  state = {
+  }
+
+  render () {
+    return (pug`
+      View(style=styles.body)
+        Text(style=styles.text) page is Login
+        Text(style=styles.text) the name is #{this.props.userInfo.username}
+    `)
+  }
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    marginTop: 20
+  }
+})
+
+export default Login
